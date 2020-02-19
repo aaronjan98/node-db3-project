8ba18d61a791bc4a63c9f8ad73bb4979175aa507
aaronjan <aaronjan98@gmail.com>
2020-02-19 13:48:30 -0800

    `add(scheme)`

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,9 @@ const db = require("../data/db-configs.js");
 
 module.exports = {
   find,
-  findById
+  findById,
+  findSteps,
+  add
 };
 
 function find() {
@@ -13,4 +15,15 @@ function findById(id) {
     return db('schemes')
             .where({ id })
             .first();
+}
+
+function findSteps(id) {
+    return db('steps as st')
+            .join('schemes as sc', 'sc.id', 'st.scheme_id')
+            .select('st.*', 'sc.scheme_name')
+            .where('scheme_id', id);
+}
+
+function add(scheme) {
+    return db('schemes').insert(scheme, 'id');
 }
